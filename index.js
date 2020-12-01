94c11899d7fa64b5cc25127ffe0fe7c165e40cfd
roberteftene <roberteftene25@gmail.com>
2020-12-01 02:16:38 +0200

    added navbar, footer and video section

---
+++ index.js
@@ -0,0 +1,20 @@
+const items = document.querySelector(".submenu");
+const submenu = document.querySelector('.has-submenu');
+const navbar = document.querySelector('.navbar');
+
+submenu.addEventListener('click', () => {
+    items.setAttribute("id", "submenu-active")
+    submenu.style.transform = "none"
+    submenu.style.border = "none"
+})
+
+document.addEventListener('click',closeSubmenu,false);
+
+/* Close dropdown */
+function closeSubmenu(e) {
+    let isClickInside = submenu.contains(e.target);
+    if (!isClickInside && submenu.querySelector("#submenu-active")) {
+        document.getElementById('submenu-active').id = ""
+        
+    }
+}
