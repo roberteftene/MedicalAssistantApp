e574e147d5a331f19a041621209a990a5f63fd85
Patricia Badea <badeapatricia18@stud.ase.ro>
2020-12-16 22:38:20 +0200

    probl-container

--- pages/AboutUs/app.js
+++ pages/AboutUs/app.js
@@ -1,55 +1,28 @@
-const image = document.getElementById("Source");
-const canvas = document.getElementById("Canvas");
-const rOffsetInput = document.getElementById("rOffset");
-const gOffsetInput = document.getElementById("gOffset");
-const bOffsetInput = document.getElementById("bOffset");
-if (image.complete) init();
-image.addEventListener("load", init);
+const btnSub = document.getElementById('btnSubmit')
+const goTopAudio = new Audio('goTop.mp3')
+const subscribeAudio = new Audio('success.mp3')
+const email = document.getElementById("email")
+const topBtn = document.getElementById("goTopBtn")
+
+function messageSubscribed() {
+  subscribeAudio.play(); 
+  email.remove();
+}
 
 
-
-
-function init() {
-    const ctx = canvas.getContext("2d");
-    const width = image.naturalWidth;
-    const height = image.naturalHeight;
-    canvas.width = 800;
-    canvas.height =500;
-    ctx.drawImage(image, 0, 0, width, height);
-    const imageData = ctx.getImageData(0, 0, width, height);
-  //  console.log(imageData);
-
-    rOffsetInput.addEventListener("change", updateCanvas);
-    gOffsetInput.addEventListener("change", updateCanvas);
-    bOffsetInput.addEventListener("change", updateCanvas);
-    
-    function updateCanvas() {
-      const updatedImageData = rgbSplit(imageData, {
-        rOffset: Number(rOffsetInput.value), 
-        gOffset: Number(gOffsetInput.value),
-        bOffset: Number(bOffsetInput.value)
-      });
-      ctx.putImageData(updatedImageData, 0, 0);
-    }
+window.onscroll = () => {
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topBtn.style.display = "block"
+  } else {
+      topBtn.style.display = "none"
   }
+}
 
-function rgbSplit(imageData, options) {
-    const { rOffset = 0, gOffset = 0, bOffset = 0 } = options; 
-    const originalArray = imageData.data;
-    const newArray = new Uint8ClampedArray(originalArray);
-    for (let i = 0; i < originalArray.length; i += 4) {
-      newArray[i + 0 + rOffset * 4] = originalArray[i + 0]; // 🔴
-      newArray[i + 1 + gOffset * 4] = originalArray[i + 1]; // 🟢
-      newArray[i + 2 + bOffset * 4] = originalArray[i + 2]; // 🔵
-    }
-    return new ImageData(newArray, imageData.width, imageData.height);
-  }
-
-  /* Demo purposes only */
-  $("figure").mouseleave(
-    function() {
-      $(this).removeClass("hover");
-    }
-  );
-
-
+function goTop() {
+  goTopAudio.play();
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTo({
+      top: 0,
+      behavior: "smooth"
+  })
+}
