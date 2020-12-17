1e88f754ee0d5dd5838d581dc8b820138b495414
roberteftene <roberteftene25@gmail.com>
2020-12-17 13:25:08 +0200

    added imageScan

--- _assets/scripts/DragAndDrop.js
+++ _assets/scripts/DragAndDrop.js
@@ -1,4 +1,7 @@
 let imageUploadContainer = document.querySelector('.image-upload');
+let radiographyCanvas = document.getElementById('radiographyCanvas');
+let context = radiographyCanvas.getContext('2d')
+let deleteImageBtn = document.querySelector('.delete-images');
 let imagesUploaded = []
 imageUploadContainer.ondragover = (e) => {
     e.preventDefault();
@@ -22,15 +25,15 @@ imageUploadContainer.ondrop = e => {
                 imagesUploaded.push(draggedImage);
                 imagesArea.appendChild(draggedImage);
                 let checkedInput = document.createElement('input')
-                checkedInput.setAttribute('type','checkbox');
+                checkedInput.setAttribute('type', 'checkbox');
                 checkedInput.id = 'checkbox-' + (imagesUploaded.length - 1);
 
                 checkedInput.addEventListener('change', (e) => {
-                    
+
                     let checkboxId = e.target.id
-                    let imgSelectedId = 'image-'+checkboxId.split('-')[1];
+                    let imgSelectedId = 'image-' + checkboxId.split('-')[1];
                     let selectedImg = document.getElementById(imgSelectedId);
-                    if(e.target.checked) {
+                    if (e.target.checked) {
                         selectedImg.selected = 1
                         deleteImageBtn.style.visibility = 'visible'
                     } else {
@@ -47,18 +50,65 @@ imageUploadContainer.ondrop = e => {
 
 function removeImages() {
     let imagesToBeRemoved = []
-    imagesUploaded.forEach( (el) => {
-        if(el.selected === 1) {
+    imagesUploaded.forEach((el) => {
+        if (el.selected === 1) {
             imagesToBeRemoved.push(el)
         }
     })
-    imagesToBeRemoved.forEach( (el) => {
+    imagesToBeRemoved.forEach((el) => {
         let selectedCheckbox = document.getElementById('checkbox-' + el.id.split('-')[1])
         selectedCheckbox.parentNode.removeChild(selectedCheckbox)
         el.parentNode.removeChild(el)
         let removedPos = imagesUploaded.indexOf(el)
-        if(removedPos > -1) {
-            imagesUploaded.splice(removedPos,1)
+        if (removedPos > -1) {
+            imagesUploaded.splice(removedPos, 1)
         }
     })
+}
+
+function getSelectedImg() {
+    let imagesSelected = []
+    imagesUploaded.forEach((el) => {
+        if (el.selected === 1) {
+            imagesSelected.push(el)
+        }
+    })
+    return imagesSelected;
+}
+
+function drawImage(img) {
+    img.forEach((el) => {
+
+        let imgHeight
+        let imgWidth
+        let x
+        let y
+        imgHeight = radiographyCanvas.height /img.length - 20;
+        imgWidth = el.width * (imgHeight / el.height);
+        // center
+        x = (radiographyCanvas.width - imgWidth) / 2
+        // margin
+        y = 10 
+        context.drawImage(el, x, y, imgWidth, imgHeight);
+    })
+
+}
+
+function drawSelectedImages() {
+    let imagesSelected = getSelectedImg();
+    if (imagesSelected.length !== 1) {
+        toastr.error('You can chose only one image for scan')
+    } else {
+        drawImage(imagesSelected);
+    }
+}
+
+function addNegativeEffect() {
+    let imgData = context.getImageData(0,0,radiographyCanvas.width,radiographyCanvas.height);
+    for(let i = 0; i < radiographyCanvas.height * radiographyCanvas.width * 4; i+= 4) {
+        imgData.data[i] = 255 - imgData.data[i];
+        imgData.data[i + 1] = 255 - imgData.data[i + 1]
+        imgData.data[i + 2] = 255 - imgData.data[i + 2]
+    }
+    context.putImageData(imgData,0,0);
 }
